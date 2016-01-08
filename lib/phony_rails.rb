7f800c7b038d4f2c18ef77e48218813a5a884640
Michele Piccirillo <michele.piccirillo@gmail.com>
2016-01-08 18:04:57 +0100

    Do not make nil invalid numbers

--- lib/phony_rails.rb
+++ lib/phony_rails.rb
@@ -29,7 +29,7 @@ module PhonyRails
     original_number = number
     number = number.clone # Just to be sure, we don't want to change the original.
     number.gsub!(/[^\(\)\d\+]/, '') # Strips weird stuff from the number
-    return if number.blank?
+    return original_number if number.blank?
     if _country_number = options[:country_number] || country_number_for(options[:country_code])
       options[:add_plus] = true if options[:add_plus].nil?
       # (Force) add country_number if missing
